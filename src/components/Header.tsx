24a7369f51d2dcf328b955469348b4d26cdb6a37
Harnoor0990 <harnoorgill200507@gmail.com>
2025-09-17 09:31:45 -0500

    Add Header component for Technical Help Desk

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+interface HeaderProps {}
+
+function Header({}: HeaderProps) {
+  return (
+    <header className="header">
+      <h1>Technical Help Desk</h1>
+      <p>Your IT support solution - Submit tickets, track progress, get help</p>
+    </header>
+  );
+}
+
+export default Header;
