18113ecad8e67697a48576d236aaf8fcfcd48fd2
Tong Xu <155984858+erichilla0525@users.noreply.github.com>
2025-09-20 12:22:41 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./src/components/Header/Header";
+import Nav from "./src/components/Navigation/Nav";
+import StatusList from "./src/components/status-list/StatusList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <StatusList />
+    </div>
+  );
+}
+
+export default App;
