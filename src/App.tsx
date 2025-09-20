64eb72ffa7cbddd2dc4d2ea805d62843776852e8
Tong Xu <txu2@rrc.ca>
2025-09-20 12:48:23 -0500

    Fixed the conflict and bug in development branch, read to merge this branch to development branch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import FAQ from "./components/FAQ/Faq";
 import Footer from "./components/Footer/Footer";
-
+import Header from "./components/Header/Header";
+import Nav from "./components/Navigation/Nav";
 import { useState } from 'react'
-import SearchBar from './components/Searchbar/Searchbar'
+import SearchBar from './components/Searchbar/Searchbar';
 import WorkOrder from './components/Ticket/Ticket'
+import StatusList from "./components/status-list/StatusList";
 
 function App() {
     return(
@@ -13,8 +15,8 @@ function App() {
             <SearchBar />
             <WorkOrder />
             <StatusList />
-            <FAQ/>
-            <Footer/>
+            <FAQ />
+            <Footer />
         </div>
   );
 }
