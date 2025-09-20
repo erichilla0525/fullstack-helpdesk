ea1e5cf974aa0ecb69ed6df9b4901e70075b5088
Harnoor0990 <156359525+Harnoor0990@users.noreply.github.com>
2025-09-20 11:50:04 -0500

    Update App.tsx
    Made changes to solve problem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,26 @@
-import "./App.css";
+import FAQ from "./components/FAQ/Faq";
+import Footer from "./components/Footer/Footer";
 import Header from "../Header/Header";
 import Nav from "../Navigation/Nav";
 import StatusList from "../status-list/StatusList";
 
+
+import { useState } from 'react'
+import SearchBar from './components/Searchbar/Searchbar'
+import WorkOrder from './components/Ticket/Ticket'
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <StatusList />
-    </div>
+    return(
+        <div>
+            <Header />
+            <Nav />
+            <SearchBar />
+            <WorkOrder />
+            <FAQ/>
+            <StatusList />
+            <Footer/>
+        </div>
   );
 }
 
-export default App;
+export default App;
