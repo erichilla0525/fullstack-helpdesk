20a7e37c46b422a18070ec5e68daa786536d590f
Tong Xu <txu2@rrc.ca>
2025-10-21 08:21:10 -0500

    Update mockedTicketData file and its repo layer.

---
+++ src/data/mockedData/mockedTicketFormData.ts
@@ -0,0 +1,22 @@
+export const ticketData = [
+    {
+        id: "01",
+        content: "Unable to login",
+        priority: "Medium",
+        status: "Pending",  
+    },
+
+    {
+        id: "02",
+        content: "Error notification",
+        priority: "Low",
+        status: "Open",  
+    },
+
+    {
+        id: "03",
+        content: "Issue unsolved",
+        priority: "high",
+        status: "Closed",  
+    }
+]
