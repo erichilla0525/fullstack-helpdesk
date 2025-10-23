d4403936095ced455f39266088654ef12b7fa246
Mowmita Yasmin <meite@academin.rrc.ca>
2025-10-23 10:45:00 -0500

    Update hook to use faqService and add search feature

--- src/components/Hooks/useFaq.tsx
+++ src/components/Hooks/useFaq.tsx
@@ -1,22 +1,35 @@
-import type { FAQItem } from "../FAQ/Faq";
-
+import { useState } from "react";
+import type { FAQItem } from "../components/FAQ/Faq";
+import * as FaqService from "../services/faqService";
 export const useFAQ = (
   faqData: FAQItem[],
   setFaqData: React.Dispatch<React.SetStateAction<FAQItem[]>>
 ) => {
-  const addFAQ = (newFAQ: FAQItem) => {
-    setFaqData((prev) => [...prev, newFAQ]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredFAQs = faqData.filter((faq) =>
+    faq.question.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const addFAQ = async (newFAQ: FAQItem) => {
+    const res = await FaqService.createFaq(newFAQ);
+    setFaqData(res);
   };
 
-  const editFAQ = (id: number, updatedFAQ: FAQItem) => {
-    setFaqData((prev) =>
-      prev.map((faq) => (faq.id === id ? { ...faq, ...updatedFAQ } : faq))
-    );
+  const editFAQ = async (id: number, updatedFAQ: FAQItem) => {
+    const res = await FaqService.updateFaq(updatedFAQ);
+    setFaqData(res);
   };
 
-  const deleteFAQ = (id: number) => {
+  const deleteFAQ = async (id: number) => {
+    await FaqService.deleteFaq(id);
     setFaqData((prev) => prev.filter((faq) => faq.id !== id));
   };
 
-  return { addFAQ, editFAQ, deleteFAQ };
+  return {
+    searchTerm,
+    setSearchTerm,
+    filteredFAQs,
+    addFAQ,
+    editFAQ,
+    deleteFAQ,
+  };
 };
