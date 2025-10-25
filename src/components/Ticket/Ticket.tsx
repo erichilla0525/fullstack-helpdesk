e5aba5916b10fd30bf8b77119e18571939978278
Tong Xu <txu2@rrc.ca>
2025-10-25 06:02:56 -0500

    Update deleteTicket logic.

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,23 +1,29 @@
 import { useTickets } from "../Hooks/ticketFormHook";
 import type { Ticket } from "../Hooks/ticketFormHook";
+import { Trash2 } from "lucide-react"
 
 export default function WorkOrder() {
 
-  const { tickets } = useTickets()
+  const { tickets, deleteTicket } = useTickets()
 
   return(
-      <div className="flex flex-col justify-center mt-5 mb-5 gap-12 ">
+      <div className="flex flex-col items-center justify-between mt-3 mb-3 gap-5">
 
         <div className='mx-auto space-y-3 border-2'>
           {tickets.map((ticket:Ticket) => (
-            <div key={ticket.id} className='bg-blue-300'>
-              <p>id: {ticket.id}</p>
-              <p>content: {ticket.content}</p>
-              <p>priority: {ticket.priority}</p>
-              <p>status: {ticket.status}</p>
+            <div>
+              <div key={ticket.id} className='bg-blue-300'>
+                <p>id: {ticket.id}</p>
+                <p>content: {ticket.content}</p>
+                <p>priority: {ticket.priority}</p>
+                <p>status: {ticket.status}</p>
+              </div>
+              
+              <button type="button" onClick={() => deleteTicket(ticket.id)} className="bg-pink-200 border-2 mt-3 mb-1 cursor-pointer">
+                  <Trash2 size={20}/>
+              </button>
             </div>
-          ))
-          }
+          ))}
         </div>
       </div> 
     );
