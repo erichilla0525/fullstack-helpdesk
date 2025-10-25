9095fdd9c6ba69a7418c10ba07451a4b1d638727
Tong Xu <txu2@rrc.ca>
2025-10-25 06:57:13 -0500

    Update css in ticket.tsx.

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -7,21 +7,24 @@ export default function WorkOrder() {
   const { tickets, deleteTicket } = useTickets()
 
   return(
-      <div className="flex flex-col items-center justify-between mt-3 mb-3 gap-5">
+      <div className="flex flex-col items-center mt-3 mb-3 gap-5">
 
         <div className='mx-auto space-y-3 border-2'>
           {tickets.map((ticket:Ticket) => (
             <div>
-              <div key={ticket.id} className='bg-blue-300'>
-                <p>id: {ticket.id}</p>
-                <p>content: {ticket.content}</p>
-                <p>priority: {ticket.priority}</p>
-                <p>status: {ticket.status}</p>
+              <div key={ticket.id} className='bg-blue-300 flex justify-between items-center'>
+                <div className="w-full">
+                  <p>id: {ticket.id}</p>
+                  <p>content: {ticket.content}</p>
+                  <p>priority: {ticket.priority}</p>
+                  <p>status: {ticket.status}</p>
+                  
+                </div>
+                <button type="button" onClick={() => deleteTicket(ticket.id)} className="bg-pink-200 border-2 mt-3 mb-1 mr-1 cursor-pointer">
+                  <Trash2 size={20}/>
+                </button>
               </div>
               
-              <button type="button" onClick={() => deleteTicket(ticket.id)} className="bg-pink-200 border-2 mt-3 mb-1 cursor-pointer">
-                  <Trash2 size={20}/>
-              </button>
             </div>
           ))}
         </div>
